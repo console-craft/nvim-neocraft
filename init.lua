067c3dc48d5ae7078dc96ad7dddee164a959d13c
Ovi Ispas <ovi@console-craft.com>
2026-03-15 13:23:52 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
